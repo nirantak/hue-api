402c25f4f8b0bab0a94cd2c75179fc49c0b98158
Nirantak Raghav <me@nirantak.com>
2021-06-14 17:23:15 +0530

    Add tests for bridge api

--- tests/test_api/test_bridge.py
+++ tests/test_api/test_bridge.py
@@ -1,3 +1,6 @@
+from unittest.mock import patch
+
+import pytest
 from faker import Faker
 
 from hue import Bridge
@@ -10,4 +13,40 @@ class TestBridge:
 
     def test_bridge_init(self):
         bridge = Bridge(ip=self.ip, user=self.user)
-        assert bridge
+        assert bridge.ip == self.ip
+        assert bridge.user == self.user
+        assert str(bridge) == f"<Bridge {self.ip}>"
+
+    def test_bridge_url(self):
+        bridge = Bridge(ip=self.ip, user=self.user)
+        assert bridge.url == f"http://{self.ip}/api/{self.user}"
+
+    @patch("hue.api.http.get_json")
+    @pytest.mark.asyncio
+    async def test_bridge_discover(self, mock_http):
+        bridge = Bridge(ip=self.ip, user=self.user)
+        await bridge.discover()
+        mock_http.assert_called_once_with("https://discovery.meethue.com/")
+        assert mock_http.call_count == 1
+
+    @patch("hue.api.http.get_json")
+    @pytest.mark.asyncio
+    async def test_bridge_info(self, mock_http):
+        mock_resp = {"state": True}
+        mock_http.return_value = mock_resp
+        bridge = Bridge(ip=self.ip, user=self.user)
+        resp = await bridge.get_info()
+        mock_http.assert_called_once_with(f"http://{self.ip}/api/{self.user}")
+        assert mock_http.call_count == 1
+        assert resp == mock_resp
+        assert bridge.info == mock_resp
+
+    @patch("hue.api.bridge.Bridge.get_info")
+    @pytest.mark.asyncio
+    async def test_bridge_config(self, mock_http):
+        mock_resp = {"config": {"state": True}}
+        mock_http.return_value = mock_resp
+        bridge = Bridge(ip=self.ip, user=self.user)
+        resp = await bridge.get_config()
+        assert mock_http.call_count == 1
+        assert resp == mock_resp["config"]
--- tests/test_api/test_light.py
+++ tests/test_api/test_light.py
@@ -11,4 +11,18 @@ class TestLight:
 
     def test_light_init(self):
         light = Light(self.num, ip=self.ip, user=self.user)
-        assert light
+        assert light.id == self.num
+        assert light.ip == self.ip
+        assert light.user == self.user
+        assert light.on is None
+        assert light.info == {}
+        assert light.state == {}
+        assert light.saved_state == {}
+        assert str(light) == f"<Light {self.num}>"
+        assert repr(light) == f"<Light id={self.num} on={None} ip={self.ip}>"
+
+    def test_light_url(self):
+        light = Light(self.num, ip=self.ip, user=self.user)
+        assert (
+            light.url == f"http://{self.ip}/api/{self.user}/lights/{self.num}"
+        )
